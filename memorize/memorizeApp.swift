a8960fda324fffffe925b814b6ded8d0641c7cbb
Kevin Sweeney <hello@kevinsweeney.io>
2024-03-22 22:47:47 -0400

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Kevin Sweeney on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
