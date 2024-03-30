92376d5244f1486bcd45665ddd9a0a68434fc537
Kevin Sweeney <hello@kevinsweeney.io>
2024-03-30 12:31:42 -0400

    Modularize view components

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -10,28 +10,65 @@ import SwiftUI
 struct ContentView: View {
     let emojis = ["😈", "🍺", "🥳", "🦉", "🏃‍♂️‍➡️", "🌯", "🌭", "🎾", "🧲"]
     @State var cardCount = 4
+    let maxCards = 8
+    
     var body: some View {
         VStack {
-            HStack {
-                ForEach(0..<cardCount, id: \.self) { index in
-                    CardView(content: emojis[index])
-                }
-            }
-            HStack {
-                Button("Add Card") {
-                    cardCount = min(emojis.count, cardCount + 1)
-                }
-                Spacer()
-                Button("Remove Card") {
-                    cardCount = max(1, cardCount - 1)
-                }
-            }
+            gameTitle
+            cardStack
+            gameActions
         }
         .foregroundColor(.orange)
         .imageScale(.small)
         .padding()
     }
+    
+    var gameTitle: some View {
+        Text("Memorize!")
+            .font(.title)
+            .foregroundStyle(.white)
+    }
+    
+    var cardStack: some View {
+        HStack {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
+        }
+    }
+    var gameActions: some View {
+        VStack {
+            cardActions
+        }
+    }
+    
+    func cardCountAdjuster(by offset:Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .imageScale(.large)
+        .disabled(cardCount <= 1 || cardCount > maxCards)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "minus.square.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "plus.square.fill")
+    }
+    
+    var cardActions: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+    }
 }
+
 
 struct CardView: View {
     let content: String
