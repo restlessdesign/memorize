89f7233f6428cba4fcd82cc84b4b06057baf4014
Kevin Sweeney <hello@kevinsweeney.io>
2024-04-05 12:03:50 -0400

    Document EmojiMemoryGame viewmodel

--- memorize/EmojiMemoryGame.swift
+++ memorize/EmojiMemoryGame.swift
@@ -1,15 +1,13 @@
-//
-//  File.swift
-//  memorize
-//
-//  Created by Kevin Sweeney on 4/1/24.
-//
-
 import Foundation
 
+/// ViewModel of an emoji-specifc version of the memory game
 class EmojiMemoryGame: ObservableObject {
+    
+    /// The set of emojis that will be used to construct cards
     private static let emojis = ["🥐", "🧀", "🌭", "🥞", "🌮", "🥧"]
     
+    /// Constructs the model to be used for this version of the game, using `emojis` as the source for card content
+    /// - Returns: An instance of a String-based memory game model
     private static func createMemoryGame() -> MemoryGame<String> {
         MemoryGame(numberOfPairsOfCards: 4) { pairIndex in
             if emojis.indices.contains(pairIndex) {
@@ -20,18 +18,23 @@ class EmojiMemoryGame: ObservableObject {
         }
     }
     
+    /// Exposes the model for the purposes of being observed, so that our view has something to react to
     @Published private var model = createMemoryGame()
     
+    /// Exposes the cards that have been constructed by our model for use in our view
     var cards: [MemoryGame<String>.Card] {
         return model.cards
     }
     
     // MARK: - Intents
     
+    /// Picks a specific card
+    /// - Parameter card: A user-selected card
     func choose(_ card: MemoryGame<String>.Card) {
         model.choose(card)
     }
     
+    /// Shuffles the cards
     func shuffle() {
         model.shuffle()
     }
