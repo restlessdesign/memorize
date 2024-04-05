8800957b68dc8941dd2f63ca96a06c80b28f9d34
Kevin Sweeney <hello@kevinsweeney.io>
2024-04-04 22:33:42 -0400

    Add additional card flipping and matching logic

--- memorize/MemoryGame.swift
+++ memorize/MemoryGame.swift
@@ -21,11 +21,25 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         }
     }
     
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter { index in cards[index].isFaceUp }.only }
+        set { cards.indices.forEach { cards[$0].isFaceUp = (newValue == $0) } }
+    }
+    
     mutating func choose(_ card: Card) {
         if let chosenIndex = cards.firstIndex(where: {$0.id == card.id}) {
-            // card is a value type here, so we can’t toggle() its .isFaceUp property!
-            // We must instead go to our cards property…
-            cards[chosenIndex].isFaceUp.toggle()
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                        cards[chosenIndex].isMatched = true
+                        cards[potentialMatchIndex].isMatched = true
+                    }
+                }
+                else {
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
+            }
         }
     }
     
@@ -54,5 +68,11 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         var isMatched = false
         
         let id: String
+    }
+}
+
+extension Array {
+    var only: Element? {
+        count == 1 ? first : nil
     }
 }
